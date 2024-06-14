3506e05f7ce6092b71ed7c303a9fa0818ced55f2
HowieHz <howie_hzgo@outlook.com>
2024-06-15 01:27:19 +0800

    perf: 调大默认值，让叠键更不容易出现

--- src/options_default/mania_2k_options_default.py
+++ src/options_default/mania_2k_options_default.py
@@ -3,6 +3,6 @@ from custom_types import Mania2kOptions
 mania_2k_options_default: Mania2kOptions = {
     "start_key": 1,
     "trill_start_key": 1,
-    "minimum_jack_time_interval": 150.0,  # 100 bpm jack ?
+    "minimum_jack_time_interval": 200,
     "maximum_number_of_jack_notes": 1,
 }
