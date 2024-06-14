f6b097059e29cea0bf3dbfeaf700271b55df88c1
HowieHz <howie_hzgo@outlook.com>
2024-06-14 12:02:17 +0800

    refactor: 重构 logger.debug，使其输出更易读

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,10 +1,15 @@
-import os
+import os, pprint
 
-DEBUG_FLAG: bool = bool(os.getenv('DEBUG_FLAG'))  # 有文字都会转换成 true，所以不用担心报错
+DEBUG_FLAG: bool = bool(
+    os.getenv("DEBUG_FLAG")
+)  # 有文字都会转换成 true，所以不用担心报错
 
-def debug(message: str, debug_flag: bool = False) -> None:
-    if DEBUG_FLAG or debug_flag: 
-        print(f"[debug/开发分析]: {message}")
+
+def debug(message: str, data: any = "", debug_flag: bool = False, end: str | None = "\n") -> None:
+    if DEBUG_FLAG or debug_flag:
+        print(f"[debug/开发分析]: {message}", end=end)
+        pprint.pprint(data, indent=4, width=160, sort_dicts=False)
+
 
 def info(message: str) -> None:
     print(f"[info/信息]: {message}")
--- src/reader/hit_objects_parser.py
+++ src/reader/hit_objects_parser.py
@@ -12,15 +12,26 @@ from custom_types import HitObject
 # om长键语法： x,y,开始时间,物件类型,长键音效,结束时间,长键音效组
 # mania y 有看见固定 0，也有看见固定 192，好像无所谓，4k x有 64 192 320 448
 
+# 打击物件语法：x,y,时间,物件类型,打击音效,物件参数,打击音效组
+
+# x（整型） 和 y（整型）： 物件的位置，原点在左上角，单位是 osu! 像素。
+# 时间（整型）： 物件精确击打的时间。以谱面音频开始为原点，单位是毫秒。
+# 物件类型（整型）： 一位标记物件类型的数据。参见：类型部分。
+# 打击音效（整型）： 一位标记物件打击音效的数据。参见：音效部分。
+# 物件参数（逗号分隔的数组）： 根据物件类型不同附加的一些参数。
+# 打击音效组（冒号分隔的数组）： 击打物件时，决定具体该播放哪些音效的一些参数。与打击音效参数关系密切。参见：音效部分。如果没有设置特殊参数，则默认为 0:0:0:0:。
+
 # 类型
 # 物件类型参数是一个 8 位整数，每一位都有特殊的含义。
 # 位次序	含义
-# 0	将物件标记为圆圈
-# 1	将物件标记为滑条
-# 2	标记新 Combo 起始物件
-# 3	将物件标记为转盘
-# 4, 5, 6	一个 3 位整数，指定要跳过多少 Combo 颜色（即“跳过连击色 (Colour hax)”）。仅在物件位于新 Combo 开始时才相关。
-# 7	将物件标记为 osu!mania 长按音符
+# 0	将物件标记为圆圈 -1
+# 1	将物件标记为滑条 -2
+# 2	标记新 Combo 起始物件 -3
+# 3	将物件标记为转盘 -4
+# 4, 5, 6	一个 3 位整数，指定要跳过多少 Combo 颜色（即“跳过连击色 (Colour hax)”）。仅在物件位于新 Combo 开始时才相关。 -5 -6 -7
+# 7	将物件标记为 osu!mania 长按音符 -8
+# 0 位是最低位
+
 
 def hit_objects_parser(hit_objects_list: list[str]) -> list[HitObject]:
     """解析 [HitObjects] 下每行的数据为更易于处理的形式
@@ -31,7 +42,47 @@ def hit_objects_parser(hit_objects_list: list[str]) -> list[HitObject]:
     Returns:
         list[HitObject]: 一个列表，装了解析后的铺面描述
     """
+    rt_list: list[HitObject] = []
+
     for hit_object in hit_objects_list:
+        type: str = ""
+        start_time: int = 0  # 毫秒
+        end_time: int = 0
+
         object_params: list[str] = hit_object.split(",")
-        debug(f"object_params: {object_params}")
-        debug(bin(int(object_params[3])))
+        debug("object_params", data=object_params)
+        raw_type: str = (
+            str(bin(int(object_params[3]))).removeprefix("0b").zfill(8)
+        )  # 处理下数据，十进制转二进制，然后去掉左边 0b 标识，补齐八位避免 IndexError，转换成字符串方便直接取位值
+        debug("raw_type", data=raw_type)
+
+        if raw_type[-1] == "1":
+            type = "hit circle"
+            start_time = end_time = int(object_params[2])
+            debug("hit circle time", data=start_time)
+        elif raw_type[-2] == "1":
+            type = "slider"
+            start_time = int(object_params[2])
+            debug("slider start time", data=start_time)
+            end_time = start_time + 100  # TODO : 分析滑条结束时间
+            debug("spinner end time", data=end_time)
+        elif raw_type[-4] == "1":
+            type = "spinner"
+            start_time = int(object_params[2])
+            end_time = int(object_params[5])
+            debug("spinner start time", data=start_time)
+            debug("spinner end time", data=end_time)
+        elif raw_type[-8] == "1":
+            type = "hold"
+            start_time = int(object_params[2])
+            end_time = int(object_params[5])
+            debug("hold start time", data=start_time)
+            debug("hold end time", data=end_time)
+        else:
+            type = "unknown"
+
+        rt_list.append({"type": type, "start_time": start_time, "end_time": end_time})
+
+    debug("rt_list", data=rt_list)
+
+    return rt_list
