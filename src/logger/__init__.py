8b73b50f6efee316ed62b741809f63b2bd2ca342
HowieHz <howie_hzgo@outlook.com>
2024-06-22 12:00:22 +0800

    style: 格式化代码

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -8,12 +8,13 @@ DEBUG_FLAG: bool = bool(
 if DEBUG_FLAG:
     log_stream = open("debug.log", mode="a+", encoding="utf-8")
 
-def debug(
-    message: str, data: any = "", end: str | None = "\n"
-) -> None:
+
+def debug(message: str, data: any = "", end: str | None = "\n") -> None:
     if DEBUG_FLAG:
         log_stream.write(f"[debug/开发分析]: {message}\n")
-        log_stream.write(pprint.pformat(data, indent=4, width=160, sort_dicts=False)+"\n")
+        log_stream.write(
+            pprint.pformat(data, indent=4, width=160, sort_dicts=False) + "\n"
+        )
 
 
 def info(message: str) -> None:
