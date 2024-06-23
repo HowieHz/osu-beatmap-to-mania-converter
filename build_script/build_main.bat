058392553561683a4f774ae1948f7becad004d0e
HowieHz <howie_hzgo@outlook.com>
2024-06-24 01:11:59 +0800

    chore: 修改构建脚本

--- build_script/build_main.bat
+++ build_script/build_main.bat
@@ -1,3 +1,5 @@
+del .\dist\obmc-latest.exe
+del .\dist\obmc-latest-noconsole.exe
 pyinstaller .\src\main.py --onefile
 ren .\dist\main.exe obmc-latest.exe
 pyinstaller .\src\main.py --onefile --noconsole
