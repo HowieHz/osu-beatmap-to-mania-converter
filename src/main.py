da20b999d5b7daaf5e496b43b706d6f4c9734353
HowieHz <howie_hzgo@outlook.com>
2024-06-23 23:24:03 +0800

    fix: 修复打包后文件不可用的问题

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import multiprocessing
+
 from cli import cli_main
 from cui import cui_main
 from webui import webui_main
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     ret: str = cli_main()
     if ret == "enter-cui":
         cui_main()
