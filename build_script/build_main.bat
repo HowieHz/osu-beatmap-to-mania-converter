75e5b7e36823d54494ba20107b9f76cf641f3640
HowieHz <howie_hzgo@outlook.com>
2024-12-20 23:20:23 +0800

    chore: 更新构建脚本

--- build_script/build_main.bat
+++ build_script/build_main.bat
@@ -1,6 +1,9 @@
 del .\dist\obmc-latest.exe
 del .\dist\obmc-latest-noconsole.exe
+del .\dist\obmc-beatmap-difficulty-data-chart-generator-latest.exe
 pyinstaller .\src\main.py --onefile
 ren .\dist\main.exe obmc-latest.exe
 pyinstaller .\src\main.py --onefile --noconsole
 ren .\dist\main.exe obmc-latest-noconsole.exe
+pyinstaller .\src\main_generate_beatmap_difficulty_data_chart.py --onefile
+ren .\dist\main_generate_beatmap_difficulty_data_chart.exe obmc-beatmap-difficulty-data-chart-generator-latest.exe
